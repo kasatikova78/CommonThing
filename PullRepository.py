791dcf10bdef6f76148f996ecfba55bf539005ac
fddsfs <fasdDAS@gmail.com>
2021-03-21 13:33:42 +0000

    Update PullRepository.py

--- PullRepository.py
+++ PullRepository.py
@@ -6,13 +6,13 @@ import traceback
 import shutil
 
 def CloneRepository(remote_repository_path, local_repository_path):
-    print "Cloning:", local_repository_path
+    print("Cloning:", local_repository_path)
     while True:
         try:
             git.Repo.clone_from(remote_repository_path, local_repository_path)
             break
         except Exception as exception:
-            print traceback.format_exc()
+            print(traceback.format_exc())
             if os.path.exists(local_repository_path):
                 shutil.rmtree(local_repository_path)
           
@@ -21,15 +21,15 @@ def PullRepository(remote_repository_path, local_repository_path):
         CloneRepository(remote_repository_path, local_repository_path)
     else:
         try:
-            print "Start Load Local Repository:", local_repository_path
+            print("Start Load Local Repository:", local_repository_path)
             local_repository = git.Repo(local_repository_path)
-            print "Start Pull:", local_repository_path
+            print("Start Pull:", local_repository_path)
             if local_repository.is_dirty():
                 local_repository.index.checkout(force = True)
             origin = local_repository.remotes.origin
             origin.pull()
         except Exception as exception:
-            print traceback.format_exc()
+            print(traceback.format_exc())
             if os.path.exists(local_repository_path):
                 shutil.rmtree(local_repository_path)
             CloneRepository(remote_repository_path, local_repository_path)
